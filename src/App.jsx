5cbe9b5f8a200a49901f915bb1b4562204f2ea7a
M. Nadhir Fauzananto <nadhir.fauzananto@gmail.com>
2026-06-16 08:11:51 +0200

    add preload images to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,37 @@ import Technology from './components/Technology.jsx'
 
 export default function App() {
 
+  React.useEffect(() => {
+    const images = [
+      import.meta.env.BASE_URL + 'home/background-home-mobile.jpg',
+      import.meta.env.BASE_URL + 'home/background-home-tablet.jpg',
+      import.meta.env.BASE_URL + 'home/background-home-desktop.jpg',
+      import.meta.env.BASE_URL + 'destination/background-destination-mobile.jpg',
+      import.meta.env.BASE_URL + 'destination/background-destination-tablet.jpg',
+      import.meta.env.BASE_URL + 'destination/background-destination-desktop.jpg',
+      import.meta.env.BASE_URL + 'destination/image-moon.png',
+      import.meta.env.BASE_URL + 'destination/image-mars.png',
+      import.meta.env.BASE_URL + 'destination/image-europa.png',
+      import.meta.env.BASE_URL + 'destination/image-titan.png',
+      import.meta.env.BASE_URL + 'crew/background-crew-mobile.jpg',
+      import.meta.env.BASE_URL + 'crew/background-crew-tablet.jpg',
+      import.meta.env.BASE_URL + 'crew/background-crew-desktop.jpg',
+      import.meta.env.BASE_URL + 'crew/image-douglas-hurley.png',
+      import.meta.env.BASE_URL + 'crew/image-mark-shuttleworth.png',
+      import.meta.env.BASE_URL + 'crew/image-victor-glover.png',
+      import.meta.env.BASE_URL + 'crew/image-anousheh-ansari.png',
+      import.meta.env.BASE_URL + 'technology/background-technology-mobile.jpg',
+      import.meta.env.BASE_URL + 'technology/background-technology-desktop.jpg',
+      import.meta.env.BASE_URL + 'technology/image-launch-vehicle-portrait.jpg',
+      import.meta.env.BASE_URL + 'technology/image-spaceport-portrait.jpg',
+      import.meta.env.BASE_URL + 'technology/image-space-capsule-portrait.jpg',
+    ]; 
+    images.forEach((src) => {
+      const img = new Image();
+      img.src = src;
+    });
+  }, []);
+
   return (
     <HashRouter>
       <Navbar />
